aec6785579e318cc296a0fb768186db1310e656a
grishasolakhyan <borsch.tv@mail.ru>
2021-06-15 21:24:46 +1000

    Hello, World!

--- MicroVR/ElectronMicro/Assets/EleMic/ZoomCamera.cs
+++ MicroVR/ElectronMicro/Assets/EleMic/ZoomCamera.cs
@@ -21,15 +21,11 @@ public class ZoomCamera : MonoBehaviour
     private float yaw=0.0f;
     private float pitch=0.0f;
 
-    private double ProX;
-    private double ProY;
-    private double ProZ;
+    private float ProX;
+    private float ProY;
+    private float ProZ;
 
-    private float ProX1;
-    private float ProY1;
-    private float ProZ1;
-
-    public double speed = .2f;
+    public float speed = .2f;
 
     private Quaternion t;
     private Vector3 ang;
@@ -38,7 +34,7 @@ public class ZoomCamera : MonoBehaviour
     {
         Application.targetFrameRate = 60;
         startingPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        startingRot = transform.rotation.eulerAngles; 
+        //startingRot = transform.rotation.eulerAngles; 
     }
 
     private void Update()
@@ -48,52 +44,55 @@ public class ZoomCamera : MonoBehaviour
         currentY = transform.position.y;
         currentZ = transform.position.z;
         //вперёд и назад
-        ProX = speed * Math.Cos(transform.rotation.eulerAngles.x * Math.PI / 180)* Math.Cos(transform.rotation.eulerAngles.y * Math.PI / 180);
-        ProX1 = (float)ProX;
-        ProY = speed * Math.Sin(transform.rotation.eulerAngles.x * Math.PI / 180);
-        ProY1 = (float)ProY;
-        ProZ = speed * Math.Cos(transform.rotation.eulerAngles.x * Math.PI / 180) * Math.Sin(transform.rotation.eulerAngles.y * Math.PI / 180);
-        ProZ1 = (float)ProZ;
+        ProX = speed;
+        ProY = speed;
+        ProZ = speed;
 
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.E))
         {
-            currentX += ProZ1;
-            currentY -= ProY1;
-            currentZ += ProX1;
+            currentZ += ProZ;
             GetComponent<Transform>().position = new Vector3(currentX, currentY, currentZ);
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.Q))
         {
-            currentX -= ProZ1;
-            currentY += ProY1;
-            currentZ -= ProX1;
+            currentZ -= ProZ;
             GetComponent<Transform>().position = new Vector3(currentX, currentY, currentZ);
         }
 
         //влево и вправо
         if (Input.GetKey(KeyCode.A))
         {
-            currentX -= ProX1;
-            currentZ += ProZ1;
+            currentX -= ProX;
             GetComponent<Transform>().position = new Vector3(currentX, currentY, currentZ);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            currentX += ProX1;
-            currentZ -= ProZ1;
+            currentX += ProX;
             GetComponent<Transform>().position = new Vector3(currentX, currentY, currentZ);
         }
 
-        if (Input.GetMouseButton(2))
+        //вверх и вниз
+        if (Input.GetKey(KeyCode.W))
+        {
+            currentY += ProY;
+            GetComponent<Transform>().position = new Vector3(currentX, currentY, currentZ);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            currentY -= ProY;
+            GetComponent<Transform>().position = new Vector3(currentX, currentY, currentZ);
+        }
+
+        /*if (Input.GetMouseButton(2))
         {
             yaw += Hspeed * Input.GetAxis("Mouse X");
             pitch -= Vspeed * Input.GetAxis("Mouse Y");
             transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
-        }
+        }*/
 
         currentX = Mathf.Clamp(currentX, minPos, maxPos);
         currentY = Mathf.Clamp(currentY, minPos, maxPos);
-        currentZ = Mathf.Clamp(currentZ, minPos, maxPos);
+        currentZ = Mathf.Clamp(currentZ, minPos, -1);
         GetComponent<Transform>().position = new Vector3(currentX, currentY, currentZ);
         ang = transform.rotation.eulerAngles;
 
@@ -102,11 +101,11 @@ public class ZoomCamera : MonoBehaviour
         {
             //GetComponent<Transform>().position = startingPos;
             //GetComponent<Transform>().rotation = Quaternion.Euler(0, 0, 0);
-            float go = 0.15f;
+            float go = 1f;
             GetComponent<Transform>().position = Vector3.Lerp(transform.position, startingPos, go);
-            t = Quaternion.Euler(0, 0, 0);
-            GetComponent<Transform>().rotation = Quaternion.Lerp(transform.rotation, t, go);
-            ang = transform.rotation.eulerAngles;
+            //t = Quaternion.Euler(0, 0, 0);
+            //GetComponent<Transform>().rotation = Quaternion.Lerp(transform.rotation, t, go);
+            //ang = transform.rotation.eulerAngles;
         }
     }
 }
